053a16aa64f7e598d8c96d190fc7d853e5357284
Bjibjihamza <hamzabjibji@gmail.com>
2026-09-21 18:50:04 +0000

    chore: reset repository - fresh start from scratch

--- apps/web/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the NextRide brand in the navbar', () => {
-  render(<App />);
-  const brand = screen.getAllByText(/Next/i).find((el) => el.closest('.navbar-brand'));
-  expect(brand).toBeInTheDocument();
-});
